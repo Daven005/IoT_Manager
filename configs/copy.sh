92875e44e152ec3542ecf6ac8e783af1d58eb6d5
David <david@norburys.me.uk>
2019-03-15 12:39:17 +0000

    Futher corrections to system config files

--- configs/copy.sh
+++ configs/copy.sh
@@ -13,6 +13,7 @@ if [ "$1" = "wkg" ] || [ "$1" = "fbk" ] ; then
     if [ "$2" = "1" ] || [ "$2" = "2" ] ; then
         cp $C/hosts.$1.$2 /etc/hosts
         cp $C/hostname.$2 /etc/hostname
+        cp $C/dnsmasq.$1.conf /etc/dnsmasq.conf
         cp $C/dhcpcd.$1.$2.conf /etc/dhcpcd.conf
     else
         ERR="Bad IOT ($2) Should be 1 or 2"
@@ -27,7 +28,8 @@ if [ "$ERR" != "-" ] ; then
 fi
 if [ "$3" = "all" ] ; then
     cp $C/dnsmasq.conf /etc
-    cp $C/dnsmasq.blacklist.conf /etc
+    cp $C/dnsmasq.blacklist.conf /etc/dnsmasq.d
+    cp $C/dnsmasq.sethost.conf /etc/dnsmasq.d
     cp $C/50-server.cnf /etc/mysql/mariadb.conf.d
     cp $C/mariadb.cnf /etc/mysql
     cp $C/smb.conf /etc/samba

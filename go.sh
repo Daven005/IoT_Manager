8581900db07a18cec3097e35318ba254bc442f75
David <david@norburys.me.uk>
2019-10-09 12:25:27 +0100

    Changed to LF ending

--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Used to start the IoT service
-# Parameter $! is the config.json file (currently pi-config.json)
+# Parameter $1 is the config.json file (currently pi-config.json)
 # --inspect used to enable debugger
 
 CONFIG=$1 /usr/bin/node --inspect /home/iot/IoT_Manager/IOT.js

61a8de4f500cb75e471898358f69885e1056cff3
David <david@norburys.me.uk>
2019-10-10 15:25:28 +0100

    Added alarm and error log tests

---
+++ alarmLog.test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var alarmLog = require("./alarmLog");
+var dbs = require('./dbSetup');
+global.config;
+require('./config').read((cfg) => {
+    global.config = cfg;
+    dbs.init(() => {
+        alarmLog.init(() => {
+            alarmLog.set(59, 2, "Solar Control");
+            console.log(`${JSON.stringify(alarmLog.mapFmt())}`);
+            console.log(`${JSON.stringify(alarmLog.getLast())}`);
+            alarmLog.set(99, 4, "Solar Control");
+            console.log(`${JSON.stringify(alarmLog.getLast())}`);
+            alarmLog.save(() => {
+                alarmLog.removeAll();
+                alarmLog.read(() => {
+                    console.log(`${JSON.stringify(alarmLog.mapFmt())}`);
+                    alarmLog.remove(0);
+                    console.log(`${JSON.stringify(alarmLog.mapFmt())}`);
+                });
+            });
+            alarmLog.remove(0);
+        });
+    });
+});
---
+++ errorLog.test.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var errorLog = require("./errorLog");
+var dbs = require('./dbSetup');
+global.config;
+require('./config').read((cfg) => {
+    global.config = cfg;
+    dbs.init(() => {
+        errorLog.init(() => {
+            errorLog.set(56, 2, "Solar Control");
+            console.log(`${JSON.stringify(errorLog.mapFmt())}`);
+            console.log(`${JSON.stringify(errorLog.getLast())}`);
+            errorLog.set(57, 4, "Solar Control");
+            console.log(`${JSON.stringify(errorLog.getLast())}`);
+            errorLog.save(() => {
+                errorLog.removeAll();
+                errorLog.read(() => {
+                    console.log(`${JSON.stringify(errorLog.mapFmt())}`);
+                    errorLog.remove(0);
+                    console.log(`${JSON.stringify(errorLog.mapFmt())}`);
+                });
+            });
+            errorLog.remove(0);
+        });
+    });
+});

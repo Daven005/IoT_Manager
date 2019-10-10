f9f4b1e6aa320de2977ffd797b4a901faa0b63b9
David <david@norburys.me.uk>
2019-10-10 15:27:53 +0100

    Added callbacks for some alarm/errorlog functions

--- alarmLog.js
+++ alarmLog.js
@@ -3,142 +3,200 @@ var fs = require('fs');
 var fileName = 'ALARMS.js';
 var alarmList = [];
 var lastUpdate = -1;
+var alarmDescriptions = [];
 
-exports.read = function () {
-  try {
-    fs.exists(fileName, function(exists) {
-      if (exists) {
-        fs.readFile(fileName, 'utf8', function(err, data) {
-          if (err) {
-            console.log(err);
-          } else {
-            if (data.length && data[0] =='[') {
-              alarmList = JSON.parse(data);
-            }
-            lastUpdate = alarmList.length-1;
-            //console.log("%d Alarms reloaded", lastUpdate);
-          }
+exports.init = function (cb) {
+    var sqlstr = "SELECT device, number, info, description FROM errorDescriptions WHERE type='alarm'"
+    db.query(sqlstr, function (err, result) {
+        if (err) {
+            console.log("init errorLog %j, %s", err, sqlstr);
+            console.log(db);
+            return;
+        }
+        result.forEach(function (descr) {
+            alarmDescriptions.push({
+                device: descr.device,
+                number: descr.number,
+                info: descr.info,
+                description: descr.description
+            });
         });
-      }
+        // console.log(alarmDescriptions);
+        if (cb) cb();
     });
-  } catch (ex) {
-    console.log("Error reading alarmlog %j", ex);
-  }
 }
 
-exports.mapFmt = function() {
-  return alarmList;
-}
-
-exports.set = function(alarm, info, device) { // Returns true if new alarm
-  try {
-    for (el in alarmList) {
-      if (alarmList[el].alarm == alarm && alarmList[el].info == info && alarmList[el].device == device) {
-        alarmList[el].last = Date.now();
-        alarmList[el].count += 1;
-        lastUpdate = el;
-	      return false;
-      }
+exports.read = function (cb) {
+    try {
+        fs.exists(fileName, function (exists) {
+            if (exists) {
+                fs.readFile(fileName, 'utf8', function (err, data) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        if (data.length && data[0] == '[') {
+                            alarmList = JSON.parse(data);
+                        }
+                        lastUpdate = alarmList.length - 1;
+                        //console.log("%d Alarms reloaded", lastUpdate);
+                    }
+                    if (cb) cb();
+                });
+            } else {
+                if (cb) cb();
+            }
+        });
+    } catch (ex) {
+        console.log("Error reading alarmlog %j", ex);
     }
-    var e = {alarm: alarm, info: info, count:1, device: device, first: Date.now(), last:Date.now()}
-    lastUpdate = alarmList.push(e)-1;
-    return true;
-  } catch(e) {
-      console.log("Can't set alarm %d, %j", alarm, e);
-  }
-  return false;
 }
 
-exports.get = function(key) {
-  return alarmList[key];
-}
-
-exports.getLast = function() {
-  if (lastUpdate >= 0)
-    return alarmList[lastUpdate];
-  else
-    return null;
-}
-
-exports.remove = function(key) {
-  alarmList.splice(key,1);
-}
-
-exports.removeAll = function(key) {
-  alarmList = [];
-  lastUpdate = -1;
-}
-
-exports.map = function() {
-  var obj = [];
-  for (_o in alarmList) {
-    obj[_o] = alarmList[_o];
-  }
-  return obj;
-}
-
-exports.sort = function(direction, column) {
-  var obj = [];
-  var func;
-  function alarmAsc(el1, el2) {
-    if (el1.alarm == el2.alarm) {
-      return el1.info - el2.info
-    }
-    return el1.alarm - el2.alarm;
-  }
-  function alarmDesc(el1, el2) {
-    if (el1.alarm == el2.alarm) {
-      return el2.info - el1.info
-    }
-    return el2.alarm - el1.alarm;
-  }
-  function countAsc(el1, el2) {
-    return el1.count - el2.count;
-  }
-  function countDesc(el1, el2) {
-    return el2.count - el1.count;
-  }
-  function lastUpdatedAsc(el1, el2) {
-    return el1.last - el2.last;
-  }
-  function lastUpdatedDesc(el1, el2) {
-    return el2.last - el1.last;
-  }
-  function deviceAsc(el1, el2) {
-    return el1.device.localeCompare(el2.device);
-  }
-  function deviceDesc(el1, el2) {
-    return -el1.device.localeCompare(el2.device);
-  }
-  switch (column) {
-  case "alarm":
-    if (direction == "Asc") func = alarmAsc; else func = alarmDesc;
-    break;
-  case "count":
-    if (direction == "Asc") func = countAsc; else func = countDesc;
-    break;
-  case "last":
-    if (direction == "Asc") func = lastUpdatedAsc; else func = lastUpdatedDesc;
-    break;
-  case "device":
-    if (direction == "Asc") func = deviceAsc; else func = deviceDesc;
-    break;
-  default:
-    console.log("Bad column %s", column);
+exports.mapFmt = function () {
     return alarmList;
-  }
-  for (_o in alarmList) {
-    obj[_o] = alarmList[_o];
-  }
-  obj.sort(func);
-  //console.log("Sorted: %j", obj);
-  return obj;
 }
 
-exports.save = function() {
-  fs.writeFile(fileName, JSON.stringify(alarmList), function(err) {
-    if (err) {
-      console.log(err);
+exports.set = function (alarm, info, device) { // Returns true if new alarm
+    function matchAlarm(el) {
+        if (el.device != device) return false;
+        if (el.number != alarm) return false;
+        if (el.info == -1) return true;
+        if (el.info != info) return false;
+        return true;
     }
-  });
+    try {
+        for (el in alarmList) {
+            if (alarmList[el].alarm == alarm && alarmList[el].info == info && alarmList[el].device == device) {
+                alarmList[el].last = Date.now();
+                alarmList[el].count += 1;
+                lastUpdate = el;
+                return false;
+            }
+        }
+        var a = {
+            alarm: alarm,
+            info: info,
+            count: 1,
+            device: device,
+            first: Date.now(),
+            last: Date.now()
+        }
+        var found = alarmDescriptions.find(matchAlarm);
+        if (!found) {
+            console.log("No matching alarm description for: ", device, alarmDescriptions, info);
+        } else {
+            a.description = found.description;
+        }
+        lastUpdate = alarmList.push(a) - 1;
+        return true;
+    } catch (e) {
+        console.error(`Can't set alarm ${alarm}, ${e}`);
+    }
+    return false;
+}
+
+exports.get = function (key) {
+    return alarmList[key];
+}
+
+exports.getLast = function () {
+    if (lastUpdate >= 0)
+        return alarmList[lastUpdate];
+    else
+        return null;
+}
+
+exports.remove = function (key) {
+    alarmList.splice(key, 1);
+}
+
+exports.removeAll = function () {
+    alarmList = [];
+    lastUpdate = -1;
+}
+
+exports.map = function () {
+    var obj = [];
+    for (_o in alarmList) {
+        obj[_o] = alarmList[_o];
+    }
+    return obj;
+}
+
+exports.sort = function (direction, column) {
+    var obj = [];
+    var func;
+
+    function alarmAsc(el1, el2) {
+        if (el1.alarm == el2.alarm) {
+            return el1.info - el2.info
+        }
+        return el1.alarm - el2.alarm;
+    }
+
+    function alarmDesc(el1, el2) {
+        if (el1.alarm == el2.alarm) {
+            return el2.info - el1.info
+        }
+        return el2.alarm - el1.alarm;
+    }
+
+    function countAsc(el1, el2) {
+        return el1.count - el2.count;
+    }
+
+    function countDesc(el1, el2) {
+        return el2.count - el1.count;
+    }
+
+    function lastUpdatedAsc(el1, el2) {
+        return el1.last - el2.last;
+    }
+
+    function lastUpdatedDesc(el1, el2) {
+        return el2.last - el1.last;
+    }
+
+    function deviceAsc(el1, el2) {
+        return el1.device.localeCompare(el2.device);
+    }
+
+    function deviceDesc(el1, el2) {
+        return -el1.device.localeCompare(el2.device);
+    }
+    switch (column) {
+        case "alarm":
+            if (direction == "Asc") func = alarmAsc;
+            else func = alarmDesc;
+            break;
+        case "count":
+            if (direction == "Asc") func = countAsc;
+            else func = countDesc;
+            break;
+        case "last":
+            if (direction == "Asc") func = lastUpdatedAsc;
+            else func = lastUpdatedDesc;
+            break;
+        case "device":
+            if (direction == "Asc") func = deviceAsc;
+            else func = deviceDesc;
+            break;
+        default:
+            console.log("Bad column %s", column);
+            return alarmList;
+    }
+    for (_o in alarmList) {
+        obj[_o] = alarmList[_o];
+    }
+    obj.sort(func);
+    //console.log("Sorted: %j", obj);
+    return obj;
+}
+
+exports.save = function (cb) {
+    fs.writeFile(fileName, JSON.stringify(alarmList), function (err) {
+        if (err) {
+            console.log(err);
+        }
+        if (cb) cb();
+    });
 }
--- logs.js
+++ logs.js
@@ -1,65 +1,100 @@
 var login = require('./login');
 
-exports.showErrors = function(request, response) {
-  if (request.query.Action == "Delete") {
-    if (login.check(request, response)) {
-      errorLog.remove(request.query.row);
-    } else return;
-  } else if (request.query.Action == "DeleteAll") {
-    if (login.check(request, response)) {
-      errorLog.removeAll();
-    } else return;
-  } else if (request.query.Action == "Read") {
-    errorLog.read();
-  } else if (request.query.Action == "Save") {
-    errorLog.save();
-  } else if (request.query.Action == "Sort") {
-    response.render('errors', {map: errorLog.sort(request.query.Direction, request.query.Column)});
-    return;
-  }
-  response.render('errors', {map: errorLog.map()});
+errorLog.init();
+alarmLog.init(); // Don't worry about callbacks
+
+exports.showErrors = function (request, response) {
+    function reload() {
+        response.render('errors', {
+            map: errorLog.map()
+        });
+    }
+    if (request.query.Action == "Delete") {
+        if (login.check(request, response)) {
+            errorLog.remove(request.query.row);
+            reload();
+        }
+    } else if (request.query.Action == "DeleteAll") {
+        if (login.check(request, response)) {
+            errorLog.removeAll();
+            reload();
+        }
+    } else if (request.query.Action == "Read") {
+        errorLog.read(() => { reload(); });
+    } else if (request.query.Action == "Save") {
+        errorLog.save(() => { reload(); });
+    } else if (request.query.Action == "Sort") {
+        response.render('errors', {
+            map: errorLog.sort(request.query.Direction, request.query.Column)
+        });
+    } else {
+        reload();
+    }
 }
 
-exports.showMobileErrors = function(request, response) {
-  var errorStr = "";
-
-  if (request.query.row) {
-    if (login.check(request, response)) {
-      errorLog.remove(request.query.row);
-      response.render('mobileErrors', {map: errorLog.mapFmt(), type:'Error', loggedIn: request.loggedIn});
+exports.showMobileErrors = function (request, response) {
+    if (request.query.row) {
+        if (login.check(request, response)) {
+            errorLog.remove(request.query.row);
+            response.render('mobileErrors', {
+                map: errorLog.mapFmt(),
+                type: 'Error',
+                loggedIn: request.loggedIn
+            });
+        }
+    } else {
+        response.render('mobileErrors', {
+            map: errorLog.map(),
+            type: 'Error',
+            loggedIn: request.loggedIn
+        });
     }
-  } else {
-    response.render('mobileErrors', {map: errorLog.mapFmt(), type:'Error', loggedIn: request.loggedIn});
-  }
 }
 
-exports.showAlarms = function(request, response) {
-  if (request.query.Action == "Delete") {
-    if (login.check(request, response)) {
-      alarmLog.remove(request.query.row);
+exports.showAlarms = function (request, response) {
+    function reload() {
+        response.render('alarms', {
+            map: alarmLog.map()
+        });
     }
-  } else if (request.query.Action == "DeleteAll") {
-    if (login.check(request, response)) {
-      alarmLog.removeAll();
+    if (request.query.Action == "Delete") {
+        if (login.check(request, response)) {
+            alarmLog.remove(request.query.row);
+            reload();
+        }
+    } else if (request.query.Action == "DeleteAll") {
+        if (login.check(request, response)) {
+            alarmLog.removeAll();
+            reload();
+        }
+    } else if (request.query.Action == "Read") {
+        alarmLog.read(() => { reload(); });
+    } else if (request.query.Action == "Save") {
+        alarmLog.save(() => { reload(); });
+    } else if (request.query.Action == "Sort") {
+        response.render('alarms', {
+            map: alarmLog.sort(request.query.Direction, request.query.Column)
+        });
     }
-  } else if (request.query.Action == "Read") {
-    alarmLog.read();
-  } else if (request.query.Action == "Save") {
-    alarmLog.save();
-  } else if (request.query.Action == "Sort") {
-    response.render('alarms', {map: alarmLog.sort(request.query.Direction, request.query.Column)});
-    return;
-  }
-  response.render('alarms', {map: alarmLog.map()});
 }
 
-exports.showMobileAlarms = function(request, response) {
-  if (request.query.Action == "Delete") {
-    if (login.check(request, response)) {
-      alarmLog.remove(request.query.row);
+exports.showMobileAlarms = function (request, response) {
+    function reload() {
+        response.render('mobileErrors', {
+            map: alarmLog.mapFmt(),
+            type: 'Alarm',
+            loggedIn: request.loggedIn
+        });
     }
-  } else if (request.query.Action == "Save") {
-    alarmLog.save();
-  }
-  response.render('mobileErrors', {map: alarmLog.mapFmt(), type:'Alarm', loggedIn: request.loggedIn});
+    if (request.query.Action == "Delete") {
+        if (login.check(request, response)) {
+            alarmLog.remove(request.query.row);
+        }
+    } else if (request.query.Action == "Save") {
+        alarmLog.save(() => {
+            reload();
+        });
+    } else {
+        reload();
+    }
 }
